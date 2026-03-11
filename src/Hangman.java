c87ff002c24cb7368fbec7a67e4ec2247396d62e
Nssstiis <nssstiis@mail.ru>
2026-03-11 22:44:17 +0300

    style: use specific imports instead of *

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,7 +1,11 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Hangman {
     public static final int MAX_MISTAKES = 6;
